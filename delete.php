3c827e00b429847e62b5757b89756c43b1864d88
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-17 09:35:35 +0200

    WIP: Add the file delete ( to removing the data )

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['delete'])) {
+    $id = $_POST['id'];
+
+    $query = "DELETE FROM users WHERE id=$id";
+    $result = $conn->query($query);
+
+    if ($result) {
+        echo "Record deleted successfully!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
+<form method="post" action="">
+    <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+    <button type="submit" name="delete">Delete</button>
+</form>
