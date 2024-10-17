987d6dd222da4621dc913ed1da33eee8b0ab9403
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2024-10-17 09:30:47 +0200

    WIP: Add the create file ( to insert data into the database )

---
+++ create.php
@@ -0,0 +1,19 @@
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$query = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+$result = $conn->query($query);
+
+if ($result) {
+echo "Record added successfully!";
+} else {
+echo "Error: " . $conn->error;
+}
+}
+?>
+<form method="post" action="">
+    Name: <input type="text" name="name" required>
+    Email: <input type="email" name="email" required>
+    <button type="submit" name="submit">Submit</button>
+</form>
